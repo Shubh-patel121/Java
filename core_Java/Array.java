1c4c7e62e38cdf0d8a00634079e7ecb9ca2135d5
Shubh-patel121 <shubham.patidar2013@gmail.com>
2024-05-18 12:07:17 +0530

    [updated array]: multidimensional array and advanced for loop in java

--- core_Java/Array.java
+++ core_Java/Array.java
@@ -7,7 +7,7 @@ public class Array {
     /*
      * Arrays are used to represent a group of elements as a single entity, but these
      * elements are homogeneous and of fixed size.
-     * - The size of the array is fixed, meaning once created, it cannot be resized.
+     * - limitations:- The size of the array is fixed, meaning once created, it cannot be resized.
      * - Arrays in Java are zero-indexed, with the first element stored at index 0.
      */
 
@@ -21,14 +21,29 @@ public class Array {
         secondArray[1] = 20;
 
         // Creating a multidimensional array
-        int[][] multiDimensionalArray = new int[4][3];
+        // 2D array -
+        int[][] twoD = new int[4][3];
+
+        // 3D array -
+        int [][][] threeD = new int[5][3][2];
+
+        //IMP:- Each time to increase or decrease the depth of the array
+        // change square brackets as per the depth
 
         // Print arrays
         System.out.println("First Array: " + Arrays.toString(firstArray));
         System.out.println("Second Array: " + Arrays.toString(secondArray));
-        System.out.println("Multidimensional Array: " + Arrays.deepToString(multiDimensionalArray));
-        
+        System.out.println("2d Array: " + Arrays.deepToString(twoD));
+        System.out.println("3d Array: " + Arrays.deepToString(threeD));
         // Jagged array: Array with varying internal array sizes
         // int[][] jaggedArray = new int[4][]; 
+
+        // ## ForEach loop :-
+        // better than for loop , it will give directly that item, no need to iterate using iterator like i, start and end;  
+        for(int n : secondArray){
+            System.out.println(n);
+        }
     }
 }
+
+// Exceptions - are the run time errors, this will not break anything while compilation;
